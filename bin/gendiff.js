726e81bf4ee1210c7fdebc94191b10304bc8eb20
Vlad Ivankov <vlad.ivankov.96@mail.ru>
2021-10-31 21:14:02 +0400

    finished second step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,5 +3,7 @@ import { Command } from 'commander/esm.mjs';
 const program = new Command();
 program
 .description('Compares two configuration files and shows a difference.')
+.arguments('<filepath1> <filepath2>')
+.option('-f, --format <type>', 'output format', 'stylish')
 .version('1.0.0', '-v, --version', 'output the version number')
 .parse(process.argv);
