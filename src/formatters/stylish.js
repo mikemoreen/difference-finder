c4b11ae21eb9bebd992458305c5b40e1fc9babc2
Vlad Ivankov <vlad.ivankov.96@mail.ru>
2021-11-08 23:37:25 +0400

    step

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -15,24 +15,24 @@ const makeString = (value, spaces) => {
 const makeStylish = (array, spaces = 0) => {
   const result = array.map((object) => {
     if (object.type === 'added') {
-      return `${makeSpace(spaces+1)}+ ${object.key}: ${makeString(object.value, spaces)}`;
+      return `${makeSpace(spaces + 1)}+ ${object.key}: ${makeString(object.value, spaces)}`;
     }
     if (object.type === 'removed') {
-      return `${makeSpace(spaces+1)}- ${object.key}: ${makeString(object.value, spaces)}`;
+      return `${makeSpace(spaces + 1)}- ${object.key}: ${makeString(object.value, spaces)}`;
     }
     if (object.type === 'parent') {
-      return ` ${makeSpace(spaces+1)} ${object.key}: {\n${makeStylish(object.children, spaces + 2)}\n${makeSpace(spaces + 2)}}`;
+      return ` ${makeSpace(spaces + 1)} ${object.key}: {\n${makeStylish(object.children, spaces + 2)}\n${makeSpace(spaces + 2)}}`;
     }
     if (object.type === 'unchanged') {
-      return `${makeSpace(spaces+1)}  ${object.key}: ${makeString(object.value, spaces)}`;
+      return `${makeSpace(spaces + 1)}  ${object.key}: ${makeString(object.value, spaces)}`;
     }
     if (object.type === 'changed') {
-      return `${makeSpace(spaces+1)}- ${object.key}: ${makeString(object.oldValue, spaces)}\n${makeSpace(spaces+1)}+ ${object.key}: ${makeString(object.newValue, spaces)}`;
+      return `${makeSpace(spaces + 1)}- ${object.key}: ${makeString(object.oldValue, spaces)}\n${makeSpace(spaces + 1)}+ ${object.key}: ${makeString(object.newValue, spaces)}`;
     }
   }).join('\n');
 
   return result;
 };
 
-const stylish = (tree)=>`{\n${makeStylish(tree, 0)}\n}`
+const stylish = (tree) => `{\n${makeStylish(tree, 0)}\n}`;
 export default stylish;
